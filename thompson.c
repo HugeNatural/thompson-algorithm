3b4f9ddf9b774020394246a86e8a5800dec81795
daxianhu <daxianhu1993@gmail.com>
2016-03-18 20:58:36 +0800

    simple thompson algorithm

---
+++ thompson.c
@@ -0,0 +1,61 @@
+//
+// Created by HugeNatural on 3/18/16.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int match(char *regexp, char *text);
+int matchhere(char *regexp, char *text);
+int matchstar(int c, char *regexp, char *text);
+
+/* match: search for regexp anywhere in text */
+int match(char *regexp, char *text)
+{
+    if (regexp[0] == '^')
+        return matchhere(regexp + 1, text);
+    do {			/* must look even if string is empty */
+        if (matchhere(regexp, text))
+            return 1;
+    } while (*text++ != '\0');
+    return 0;
+}
+
+/* matchhere: search for regexp at beginning of text */
+int matchhere(char *regexp, char *text)
+{
+    if (regexp[0] == '\0')
+        return 1;
+    if (regexp[1] == '*')
+        return matchstar(regexp[0], regexp + 2, text);
+
+    if (regexp[0] == '$' && regexp[1] == '\0')
+        return *text == '\0';
+    if (*text != '\0' && (regexp[0] == '.' || regexp[0] == *text))
+        return matchhere(regexp + 1, text + 1);
+    return 0;
+}
+
+/* matchstar: search for c*regexp at beginning of text */
+int matchstar(int c, char *regexp, char *text)
+{
+    do {			/* a * matches zero or more instances */
+        if (matchhere(regexp, text))
+            return 1;
+    } while (*text != '\0' && (*text++ == c || c == '.'));
+    return 0;
+}
+
+int main(void)
+{
+    if (match("ab*", "one two three ab")) {
+        printf("Match\n");
+        return 1;
+    }
+    else {
+        printf("No Match\n");
+        return 0;
+
+    }
+
+
+}
